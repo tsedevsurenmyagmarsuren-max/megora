75c8126cca1a2f225508ab3304ae6711470f45c9
Tsedevsuren Myagmarsuren <tsedevsurenmyagmarsuren@gmail.com>
2026-07-02 14:23:49 +0700

    protect test routes

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/yesh-test(.*)",
+  "/exams(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
